7e87ec6343911b222f4b04dfac5c6a18bd7bf62a
Djiré  Aboubakar Malick <aboubakarmalick.djire@edu.esiee.fr>
2024-09-28 07:31:43 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-#### Imports et définition des variables globales
+"""Imports et définition des variables globales"""
 
 
 #### Fonctions secondaires
 
 
 def artcode_i(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme itératif
+    """retourne la liste de tuples selon un algorithme itératif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -13,14 +13,23 @@ def artcode_i(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
     # votre code ici
+    char=[s[0]]
+    o=[1]
+    n=len(s)
+    k=1
+    while k<n:
+        if s[k]==s[k-1]:
+            o[-1]+=1
+        else:
+            char+=[s[k]]
+            o+=[1]
+        k=k+1
+    return list(zip(char,o))
 
-    return [ ]
 
-
-def artcode_r(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme récursif
+def artcode_r(s,lst=None):
+    """retourne la liste de tuples selon un algorithme récursif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -28,20 +37,28 @@ def artcode_r(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
     # votre code ici
-
+    if lst is None:
+        lst=[]
+    if len(s)==0:
+        return lst
+    char=s[0]
+    count=1
+    for i in range(1,len(s)):
+        if s[i]==char:
+            count+=1
+        else:
+            break
+    lst.append((char,count))
+    return artcode_r(s[count:],lst)
     # cas de base
     # recherche nombre de caractères identiques au premier
     # appel récursif
-
-    return []
-    
-
 #### Fonction principale
 
 
 def main():
+    """fct principale test le programme"""
     print(artcode_i('MMMMaaacXolloMM'))
     print(artcode_r('MMMMaaacXolloMM'))
 
